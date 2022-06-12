e0683158be0f36dc5f8bd33031952f117a8f4fb8
secondlast21 <secondlast2102@gmail.com>
2022-06-12 20:25:08 +0700

    add search nearest feature

--- app/src/main/java/com/example/capstonelocal/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/capstonelocal/data/retrofit/ApiService.kt
@@ -1,28 +1,7 @@
 package com.example.capstonelocal.data.retrofit
 
 import com.example.capstonelocal.data.response.AmbuHospResponse
-import com.example.capstonelocal.data.response.DataItem
-import retrofit2.Call
 import retrofit2.http.*
-import okhttp3.MultipartBody
-import okhttp3.RequestBody
-import retrofit2.Callback
-
-//interface ApiService {
-//    @FormUrlEncoded
-//    @POST("short/ambulance")
-//    fun nearestAmbulance (
-//        @Field("lintang") lintang: Double,
-//        @Field("bujur") bujur: Double
-//    ) : Call<List<DataItem>>
-//
-//    @FormUrlEncoded
-//    @POST("short/hospital")
-//    fun nearestHospital (
-//        @Field("lintang") lintang: Double,
-//        @Field("bujur") bujur: Double
-//    ) : Call<List<DataItem>>
-//}
 
 interface ApiService {
     @FormUrlEncoded
